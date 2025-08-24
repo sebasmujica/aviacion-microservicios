b91d8e8db42910e8e54ed0e7065f8d8767c2410b
Sebastian Mujica <sebas13mujica@gmail.com>
2025-08-24 14:33:57 -0300

    micro-servicios funcionales

--- reservas_service/app.py
+++ reservas_service/app.py
@@ -1,17 +1,21 @@
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 import os
-from funciones import buscar_reserva
+from funciones import buscar_reserva, init_db,agregar_a_db,consultar_a_db,consultar_id_vuelo
 
 load_dotenv()
 
 PORT = int(os.getenv("PORT","5003"))
 DB = "reservas.db"
 
-reservas = [
-    {"id": 1, "vuelo_id": 1, "pasajero": "Ana Gomez", "asiento": "12A"},
-    {"id": 2, "vuelo_id": 2, "pasajero": "Luis Perez", "asiento": "7C"},
-]
+sql_script = '''
+    CREATE TABLE IF NOT EXISTS reservas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pasajero TEXT NO NULL,
+    asiento TEXT NO NULL,
+    vuelo_id INTEGER NO NULL)'''
+
+init_db(DB,sql_script)
 
 app = Flask(__name__)
 
@@ -28,11 +32,14 @@ def status():
 
 @app.get("/reservas")
 def listar_reservas():
+    sql = "SELECT * FROM reservas"
+    reservas = consultar_a_db(DB,sql)
     return jsonify(reservas)
 
 @app.get("/reservas/<int:r_id>")
 def obtener_reserva(r_id):
-    reserva = buscar_reserva(reservas, r_id)
+    sql = "SELECT * FROM reservas WHERE id = ?"
+    reserva = consultar_a_db(DB,sql,(r_id,))
     if not reserva:
         return jsonify({"error":"not_found"}),404
     return jsonify(reserva)
@@ -40,59 +47,67 @@ def obtener_reserva(r_id):
 
 @app.post("/reservas")
 def crear_reserva():
+
     data = request.get_json(force=True,silent=True) or {}
 
-    vuelo_id = data.get("vuelo_id")
+    
     pasajero =(data.get("pasajero") or "").strip()
     asiento = (data.get("asiento") or "").strip()
-
-    if not pasajero or not asiento:
+    vuelo_id = int(data["vuelo_id"])
+    if not pasajero or not asiento or not vuelo_id:
         return jsonify({"error":"campos requeridos"}),400
 
-    try:
-        vuelo_id = int(data["vuelo_id"])
-        if vuelo_id <= 0 :
-            raise ValueError()
-    except Exception:
-        return jsonify({"error":"vuelo_id invalido"}),400
+    v_id = consultar_id_vuelo(vuelo_id)
+    if v_id is None:
+        return jsonify({"error":"Vuelo no encontrado o servicio no disponible"}), 404
 
-    nuevo_id = max([reserva["id"] for reserva in reservas]+ [0]) +1
-    reserva = {"id":nuevo_id,"vuelo_id":vuelo_id,"pasajero":pasajero,"asiento":asiento}
-    reservas.append(reserva)
+    nuevo_id = agregar_a_db(DB,pasajero,asiento,v_id)
+    reserva = {"id":nuevo_id,"vuelo_id":v_id,"pasajero":pasajero,"asiento":asiento}
+
     return jsonify(reserva),201
 
 @app.put("/reservas/<int:r_id>")
 def modificar_reserva(r_id):
-    reserva = buscar_reserva(reservas,r_id)
+
+    sql = "SELECT * FROM reservas WHERE id = ?"
+    sql_update = "UPDATE reservas SET pasajero= ?, asiento = ?, vuelo_id = ? WHERE id = ?"
+
+    reserva = consultar_a_db(DB,sql,(r_id,))
     if not reserva:
         return jsonify({"error":"not_found"}),404
 
     data = request.get_json(force=True,silent=True) or {}
-    if "pasajero" in data: reserva["pasajero"] = (data.get("pasajero") or "").strip()
-    if "asiento" in data: reserva["asiento"] = (data.get("asiento") or "").strip()
-    if "vuelo_id" in data:
-        try:
-            reserva_id = int(data["vuelo_id"])
-            if reserva_id <= 0:
-                raise ValueError()
-            reserva["vuelo_id"] = reserva_id
-        except Exception:
-            return jsonify({"error":"vuelo_id invalido"}), 400
-    return jsonify(reserva)
+    pasajero= (data.get("pasajero") or "").strip()
+    asiento = (data.get("asiento") or "").strip()
+    vuelo_id = data.get("vuelo_id")
+
+    if not pasajero or not asiento or not vuelo_id :
+        return jsonify({"error":"campos requeridos"}), 400
+
+    v_id = consultar_id_vuelo(vuelo_id)
+    if v_id is None:
+        return jsonify({"error":"Vuelo no encontrado o servicio no disponible"}),404
+
+    consultar_a_db(DB,sql_update,(pasajero,asiento,v_id,r_id))
+    reserva = consultar_a_db(DB,sql,(r_id,))
+
+    return jsonify(reserva), 200
 
 
 @app.delete("/reservas/<int:r_id>")
-def eliminar_reserva(r_id:int):
-    global reservas
+def eliminar_reserva(r_id):
 
-    existe = buscar_reserva(reservas, r_id)
+    sql = "SELECT * FROM reservas WHERE id = ?"
+    sql_delete = "DELETE FROM reservas WHERE id = ?"
+
+    existe = consultar_a_db(DB,sql,(r_id,))
 
     if not existe:
         return jsonify({"error":"not_found"}), 404
 
-    reservas = [reserva for reserva in reservas if reserva["id"]!= r_id]
+    consultar_a_db(DB,sql_delete,(r_id,))
 
-    return "reserva eliminada", 201
+    return "reserva eliminada", 204
 
 
 if __name__  == "__main__":
--- reservas_service/funciones.py
+++ reservas_service/funciones.py
@@ -1,5 +1,67 @@
+import sqlite3
+from dotenv import load_dotenv
+import requests,os
 
+load_dotenv()
 
+VUELOS_URL = os.getenv("VUELOS_URL","http://localhost:5002")
 
 def buscar_reserva(reservas,reserva_id:int):
     return next((reserva for reserva in reservas if reserva["id"]== reserva_id), None)
+
+def get_conn(db):
+    conn = sqlite3.connect(db)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db(db,sql_script):
+
+    conn = get_conn(db)
+    cur = conn.cursor()
+
+    cur.execute(sql_script)
+
+    conn.commit()
+    conn.close()
+
+def agregar_a_db(db,pasajero,asiento,v_id):
+
+    conn = get_conn(db)
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO reservas (pasajero, asiento, vuelo_id) VALUES (?,?,?) ", (pasajero,asiento,v_id) )
+    id_reserva = cur.lastrowid
+
+    conn.commit()
+    conn.close()
+
+    return id_reserva
+
+def consultar_a_db(db,sql, params = ()):
+
+    conn = get_conn(db)
+    cur = conn.cursor()
+
+    cur.execute(sql, params)
+    rows = cur.fetchall()
+
+    datos= [dict(row) for row in rows]
+
+    conn.commit()
+    conn.close()
+
+    return datos
+
+def consultar_id_vuelo(v_id):
+
+    URL = f"{VUELOS_URL}/vuelos/{v_id}"
+
+    try:
+        r = requests.get(URL,timeout=10)
+        if r.status_code == 404:
+            return None
+        r.raise_for_status()
+        data = r.json()
+    except requests.RequestException:
+        return None
+    return data[0].get("id")
